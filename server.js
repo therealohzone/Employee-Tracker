e262b696d7a5d4b6f42e4409698a177f75042491
johnoh <johnoh@usc.edu>
2021-11-23 19:12:52 -0500

    add to server.js logic

--- server.js
+++ server.js
@@ -15,3 +15,41 @@ connection.connect(function(err) {
     start();
 });
 
+function start() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Update employee role",
+                "Exit"
+            ]
+        })
+        .then(function(answer) {
+            if (answer.action === 'View all departments') {
+                viewDepartments();
+            } else if (answer.action === 'View all roles') {
+                viewRoles();
+            } else if (answer.action === 'View all employees') {
+                viewEmployees();
+            } else if (answer.action === 'Add a department') {
+                addDepartment();
+            } else if (answer.action === 'Add a role') {
+                addRole();
+            } else if (answer.action === 'Add an employee') {
+                addEmployee();
+            } else if (answer.action === 'Update employee role') {
+                updateRole();
+            } else if (answer.action === 'Exit') {
+                connection.end();
+            }
+        })
+}
+
