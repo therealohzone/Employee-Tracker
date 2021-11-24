03bee156e0b347799ae4daf2aa2f71a3fa4a8d60
johnoh <johnoh@usc.edu>
2021-11-23 19:06:10 -0500

    creat database

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE employee_db;
+
+INSERT INTO department (name) VALUES ("Data Analyst");
+INSERT INTO department (name) VALUES ("UI/UX");
+INSERT INTO department (name) VALUES ("IT");
+INSERT INTO department (name) VALUES ("Medical Staff);
